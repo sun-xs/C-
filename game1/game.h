3087326ed1cff8b9a35bcb618dd90c221e39dfd2
sun-xs <z99020331@163.com>
2022-02-22 20:54:23 +0800

    game

---
+++ game1/game.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define ROW 3
+#define COL 3
+
+#include <stdio.h>
+
+//ÉùÃ÷
+void InitBoard(char board[ROW][COL], int row, int col);
+void DisplayBoard(char board[ROW][COL], int row, int col);
+void PlayerMove(char board[ROW][COL], int row, int col);
