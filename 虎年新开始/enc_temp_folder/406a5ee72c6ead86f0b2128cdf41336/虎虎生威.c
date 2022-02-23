d90ed060a0c5e05350b250155a016933cb2595d6
sun-xs <z99020331@163.com>
2022-02-23 19:00:58 +0800

    虎年

---
+++ 虎年新开始/enc_temp_folder/406a5ee72c6ead86f0b2128cdf41336/虎虎生威.c
@@ -0,0 +1,116 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//int main()
+//{
+//	int i = 0;
+//	for (; i < 100; i++)
+//	{
+//		printf("%d ", i);
+//	}
+//
+//	for (; i < 100; i++)
+//	{
+//		printf("%d ", 10-i);
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	int sum = 0;
+//	int n = 0;
+//	int ret = 0;
+//	scanf_s("%d", &n);
+//
+//	for (i = 1; i <= n; i++)
+//	{
+//		int j = 0;
+//		int ret = 1;
+//		for (j = 1; j <= i; j++)
+//		{
+//			ret *= j;
+//		}
+//		sum += ret;
+//	}
+//	printf("%d\n", sum);
+//	return 0; 
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	for (; i <= 12; i++)
+//	{
+//		printf("hehe\n");
+//		arr[i] = 0;
+//	}
+//	system("pause");
+//	return 0;
+//}
+#include <assert.h>
+//char* my_strcpy(char* dest,const char* src)
+//{
+//	char* ret = dest;
+//	assert(dest != NULL);	//断言  保护指针的有效性
+//	assert(src != NULL);
+//	//把src指向的字符串拷贝到dest指向的空间，包含'\0'字符
+//	while (*dest++ = *src++)
+//	{
+//		; 
+//	}
+//	return ret;
+//	
+//
+//}
+//
+//int main()
+//{
+//	//strcpy 字符串拷贝
+//
+//	char arr1[] = "##############";
+//	char arr2[] = "bit";
+//	my_strcpy(arr1, arr2);
+//	printf("%s\n", arr1);
+//	return  0;
+// }
+
+//int main()
+//{
+//	unsigned int i;
+//	for (i = 9; i >= 0; i--)
+//	{
+//		printf("%u\n", i);
+//
+//	}
+//	return 0;
+//}
+
+#include <stdlib.h>
+//int main()
+//{
+//	char a[1000];
+//	int i;
+//	for (i = 0; i < 1000; i++)
+//	{
+//		a[i] = -1 - i;
+//	}
+//	printf("%d", strlen(a));
+//	return 0;
+//}
+#include <stdio.h>
+
+unsigned char i = 0;
+int main()
+{
+	for (i=0; i <= 255; i++)
+	{
+		printf("hello world\n");
+	}
+	return 0;
+}
