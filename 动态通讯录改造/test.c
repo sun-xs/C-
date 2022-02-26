8eee41c75325923c52d300f79e600650c101a388
sun-xs <z99020331@163.com>
2022-02-26 22:20:48 +0800

    动态通讯录

---
+++ 动态通讯录改造/test.c
@@ -0,0 +1,58 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "contact.h"
+
+void menu()
+{
+	printf("************************************\n");
+	printf("*********1.add      2.del **********\n");
+	printf("*********3.search   4.modify ******\n");
+	printf("*********5.show     6.sort *********\n");
+	printf("*********0.exit             ********\n");
+	printf("***********************************\n");
+}
+int main()
+{
+	int input = 0;
+	//创建通讯录
+	struct Contact con;	//con就是通讯录，里面保函：data指针和size，capacity
+	//初始化通讯录
+	InitContact(&con);
+	do
+	{
+		menu();
+		printf("请选择：>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case ADD:
+			AddContact(&con);
+			break;
+		case DEL:
+			DelContact(&con);
+			break;
+		case SEARCH:
+			SearchContact(&con);
+			break;
+		case MODIFY:
+			ModifyContact(&con);
+			break;
+		case SHOW:
+			ShowContact(&con);
+			break;
+		case SORT:
+			//SortContact(&con);
+			break;
+		case EXIT:
+			printf("退出通讯录\n");
+			break;
+		default:
+			//销毁通讯录-释放动态开辟的内存
+			DestroyContact(&con);
+			printf("选择错误\n");
+			break;
+		}
+	} while (input);
+
+	return 0;
+}
