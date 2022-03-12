38cf55b93f9ca1cd9bad8f1b361aea3678f3447e
sun-xs <z99020331@163.com>
2022-03-12 20:30:35 +0800

    预处理

---
+++ 预处理3/test.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//
+//int main()
+//{
+//	//printf("%d\n", __STDC__);
+//	
+//	
+//	
+//	return 0;
+//}
+
+//95%  -C
+//99%  -gcc
+
+#define MAX 100
+#define STR "hehe"
+
+#define do_forever for(;;)
+
+int main()
+{
+	/*int max = MAX;
+	printf("%d\n", max);*/
+
+	//printf("%s\n", STR);
+
+	do_forever;
+
+	return 0;
+}
